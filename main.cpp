a94527566a55c93a35c3c7c5d97e7bee72de2ba3
Cheney Tsai <cheney1003@gmail.com>
2020-02-07 00:33:31 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+ vector<int> twoSum(vector<int>& nums, int target) {
+        // hash[i] represent  index of nums's value 
+        // i  represent num's value
+        unordered_map<int, int> hash;
+        vector<int> result;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (hash.find(target - nums[i]) != hash.end()) {
+            
+                // target - nums[i]'s index is smaller , put it in the front
+                
+                result.push_back(hash[target - nums[i]]);
+                result.push_back(i);
+                return result;
+            }
+            hash[nums[i]] = i;
+        }
